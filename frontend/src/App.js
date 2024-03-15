c5a346e498a073754221b18b900f44e16f30cc9d
ChamodJayarathne <148670748+ChamodJayarathne@users.noreply.github.com>
2024-03-15 23:00:29 +0530

    first comit
    The Full code

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React from 'react';
+import Header from './components/Header'
+import AddPatient from './components/AddPatient';
+//import PateintDetailsManagement from './components/ManagePatient';
+import UpdatePatientDetails from './components/UpdatePatientDetails';
+import ManagePatient from './components/ManagePatient'
+import GeneratePDF from './components/GeneratePDF'
+import {BrowserRouter , Routes, Route}from 'react-router-dom'
+import About from './components/About';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header/>
+        <div className="components">
+          <Routes>
+
+            {/*<Route path="/" element={<AllPatients/>}/>*/}
+            <Route path="/add" element={<AddPatient/>}/>
+            {/*<Route path="/manage" element={<PateintDetailsManagement/>}/>*/}
+            <Route path="/GeneratePDF" element={<GeneratePDF/>}/>
+            <Route path="/ManagePatient" element={<ManagePatient/>}/>
+            <Route path="/Update/:id" element={<UpdatePatientDetails/>}/>
+            <Route path = "/About" element={<About/>}/>
+            
+          </Routes>
+        </div>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
